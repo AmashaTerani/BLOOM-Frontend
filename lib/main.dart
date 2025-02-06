26c2c369eda083c70ee3868e63b028992da6d79d
amashaterani-cell <amashaterani1@gmail.com>
2025-02-06 11:56:46 +0530

    testing the amsha's branch

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'My App',
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: const Color.fromARGB(255, 19, 8, 220),
       ),
       home: SplashScreen(),
     );
